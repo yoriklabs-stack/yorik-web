66ac88a460a212ccde0a5b7ecf66d1d23e9020df
karthikk153 <karthik.k153@gmail.com>
2026-08-08 16:21:24 +0530

    Add sitemap and SEO configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://yoriklabs.netlify.app',
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
     mdx(),
+    sitemap(),
   ],
-  site: 'https://yoriklabs.com',
 });
